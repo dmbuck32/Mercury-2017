be8aaf194d8d8a630f6def71c4c3364215b5155b
David-Michael Buckman <dnbuckman@mix.wvu.edu>
2017-04-15 15:14:22 -0400

    Updated the picture in the OCU with 2017 robot & enabled easy port number changing.

--- Mars_Rover_Master/Mars_Rover_OCU/obj/Debug/Mars_Rover_OCU_Content.g.i.cs
+++ Mars_Rover_Master/Mars_Rover_OCU/obj/Debug/Mars_Rover_OCU_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resources/robot2.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resources/robot.jpg")]
 
 
